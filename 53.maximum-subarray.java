2d2d4a1a6fb16ce080323b3d937384b26f2ee6c5
hsahu4012 <hrudayanandasahu3@gmail.com>
2024-06-17 21:58:54 +0530

    Maximum Subarray Sum

---
+++ 53.maximum-subarray.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            max = Math.max(sum, max);
+
+            if (sum < 0)
+                sum = 0;
+        }
+
+        return max;
+    }
+}
+// @lc code=end
